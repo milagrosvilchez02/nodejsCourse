39ca3f35644d0f1ef15c11dee8aabb6fcb563ccf
Milagros Vilchez <milagrosvilchez02@gmail.com>
2022-08-29 18:58:30 -0300

    templating engines

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,16 +1,108 @@
-const startupDebugger = require("debug")("app:startup");
-const dbDebugger = require("debug")("app:db");
+const config = require("config");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const Joi = require("joi");
+const logger = require("./logger");
 const express = require("express");
 const app = express();
 
+console.log("Application Name: " + config.get("name"));
+console.log("Mail host: " + config.get("mail.host"));
+console.log("Mail password: " + config.get("mail.password"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //middleware function - parses the incoming req with urlencoded payloads. to json format
+// with extended true we can pass arrays and complex objects using the urlencoded format.
+app.use(express.static("public")); //we can serve static content.
+app.use(helmet());
+
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
-  startupDebugger("Morgan enabled...");
+  console.log("Morgan enabled...");
 }
 
-// db work...
-dbDebugger("Connected to the database...");
+app.use(logger);
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.get("/courses/:id", (req, res) => {
+  // prints parameters.
+  // res.send(req.params.id);
+  // prints optional parameters called query.
+  // res.send(req.query);
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+  res.send(course);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.put("/courses/:id", (req, res) => {
+  // look up the course
+  // if not existing, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  // validate
+  // if invalid, return 400 - bad request
+
+  // const result = validateCourse(req.body);
+  const { error } = validateCourse(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // update course
+  course.name = req.body.name;
+  // return the updated course
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  return Joi.validate(course, schema);
+}
+
+app.delete("/courses/:id", (req, res) => {
+  // look up the course
+  // not existing, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  // delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  // return the same course
+  res.send(course);
